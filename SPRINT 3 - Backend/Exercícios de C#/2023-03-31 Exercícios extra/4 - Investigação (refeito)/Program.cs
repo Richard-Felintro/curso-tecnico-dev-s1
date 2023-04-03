ae03e28dc596644f13be0ffd1415f441cfce1aa0
Richard Felintro <richardfelintro06@gmail.com>
2023-04-03 17:16:33 -0300

    Exercício refeito

---
+++ SPRINT 3 - Backend/Exercícios de C#/2023-03-31 Exercícios extra/4 - Investigação (refeito)/Program.cs
@@ -0,0 +1,39 @@
+﻿int suspeita = 0;
+Console.WriteLine($@"
+ ___  ________   ___      ___ _______   ________  _________  ___  ________  ________  ________  ________  ________     
+|\  \|\   ___  \|\  \    /  /|\  ___ \ |\   ____\|\___   ___\\  \|\   ____\|\   __  \|\   ____\|\   __  \|\   __  \    
+\ \  \ \  \\ \  \ \  \  /  / | \   __/|\ \  \___|\|___ \  \_\ \  \ \  \___|\ \  \|\  \ \  \___|\ \  \|\  \ \  \|\  \   
+ \ \  \ \  \\ \  \ \  \/  / / \ \  \_|/_\ \_____  \   \ \  \ \ \  \ \  \  __\ \   __  \ \  \    \ \   __  \ \  \\\  \  
+  \ \  \ \  \\ \  \ \    / /   \ \  \_|\ \|____|\  \   \ \  \ \ \  \ \  \|\  \ \  \ \  \ \  \____\ \  \ \  \ \  \\\  \ 
+   \ \__\ \__\\ \__\ \__/ /     \ \_______\____\_\  \   \ \__\ \ \__\ \_______\ \__\ \__\ \_______\ \__\ \__\ \_______\
+    \|__|\|__| \|__|\|__|/       \|_______|\_________\   \|__|  \|__|\|_______|\|__|\|__|\|_______|\|__|\|__|\|_______|
+                                          \|_________|                                                                 ");
+Console.WriteLine($@"Bom dia, gostaria de fazer algumas perguntas ao senhor sobre o ocorrido, 
+responda apenas com true ou false.");
+Console.WriteLine($"Telefonou para a vítima?");
+bool q1 = bool.Parse(Console.ReadLine());
+if (q1 == true) {suspeita++;}
+Console.WriteLine($"Esteve no local do crime?");
+bool q2 = bool.Parse(Console.ReadLine());
+if (q2 == true) {suspeita++;}
+Console.WriteLine($"Mora perto da vítima?");
+bool q3 = bool.Parse(Console.ReadLine());
+if (q3 == true) {suspeita++;}
+Console.WriteLine($"Devia para a vítima?");
+bool q4 = bool.Parse(Console.ReadLine());
+if (q4 == true) {suspeita++;}
+Console.WriteLine($"Já trabalhou com a vítima?");
+bool q5 = bool.Parse(Console.ReadLine());
+if (q5 == true) {suspeita++;}
+if (suspeita == 2) {
+    Console.WriteLine($"~~~~~ Você é suspeito ~~~~~");
+}
+else if (suspeita == 3 || suspeita == 4) {
+    Console.WriteLine($"~~~~~ Você é um cumplice ~~~~~");
+}
+else if (suspeita == 5) {
+    Console.WriteLine($"~~~~~ VOCÊ É O CULPADO! ~~~~~");
+}
+else {
+    Console.WriteLine($"~~~~~ Você é inocente! ~~~~~");
+}
