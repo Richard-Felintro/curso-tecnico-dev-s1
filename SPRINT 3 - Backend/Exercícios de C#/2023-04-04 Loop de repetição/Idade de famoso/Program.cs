e0fdcb72b08a49b1ca3b13de7a91a2267aa32e7a
Richard Felintro <richardfelintro06@gmail.com>
2023-04-05 17:21:27 -0300

    Mudanças leves

---
+++ SPRINT 3 - Backend/Exercícios de C#/2023-04-04 Loop de repetição/Idade de famoso/Program.cs
@@ -0,0 +1,9 @@
+﻿int idade = 0;
+while (idade != 92){
+    Console.WriteLine($"Qual a idade do Sílvio Santos?");
+    idade = int.Parse(Console.ReadLine());
+    if (idade != 92){
+        Console.WriteLine($"ERROU!!!!");
+    }
+}
+Console.WriteLine($@"-+-+-CORRETO-+-+-"); 
