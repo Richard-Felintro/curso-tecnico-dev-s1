9b16c58313ccb700a7aa7ff4f29682daf01e3e71
Richard-Felintro <96843832+Richard-Felintro@users.noreply.github.com>
2023-04-10 17:15:04 -0300

    Pasta SPRINT 2 duplicata deletada

--- SPRINT 2/Projeto Gamer old/js/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarMenu() {
-    document.getElementById("menu").classList.toggle("mostrar-menu");
-}
-
-function mostrarMenuUsuario() {
-    document.getElementById("user-menu").classList.toggle("mostrar-menu-usuario");
-}
