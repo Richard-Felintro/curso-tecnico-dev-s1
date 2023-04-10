4cba2471c69d992675a6fcc5dc200459726f42c3
Richard-Felintro <96843832+Richard-Felintro@users.noreply.github.com>
2023-04-10 17:16:22 -0300

    Deletado exercício duplicado de repetição

--- SPRINT 3 - Backend/Exercícios de C#/2023-04-04 Repetição/Idade de famoso/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int idade = 0;
-while (idade != 92){
-    Console.WriteLine($"Qual a idade do Sílvio Santos?");
-    idade = int.Parse(Console.ReadLine());
-    if (idade != 92){
-        Console.WriteLine($"ERROU!!!!");
-    }
-}
-Console.WriteLine($@"-+-+-CORRETO-+-+-"); 
