13486e9fe1e3e112cd668511ac1e150f3c9a0f26
Richard Felintro <richardfelintro06@gmail.com>
2023-04-11 17:23:10 -0300

    Exercício 4 adicionado

---
+++ SPRINT 3 - Backend/Exercícios de C#/2023-04-11 Exercícios de array/4 - Números pares e impares/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numeros = new int[6];
+int pares = 0;
+int impares = 0;
+for (var i = 0; i < 6; i++)
+{
+    Console.WriteLine($"Informe um valor: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+foreach (var numero in numeros)
+{
+    if (numero % 2 == 0){
+        pares++;
+    }
+    else{
+        impares++;
+    }
+}
+Console.WriteLine($"Quantidade de valores pares: {pares}");
+Console.WriteLine($"Quantidade de valores impares: {impares}");
