70071344d3f744b0748c2da56c11fb8a07c8b8a0
Richard Felintro <richardfelintro06@gmail.com>
2023-04-14 17:05:44 -0300

    Mudanças leves

--- SPRINT 3 - Backend/Exercícios de C#/2023-04-12 Exercícios de função/3 - Lista de exercícios 13-04-2023/4 - Maior e menor/Program.cs
+++ SPRINT 3 - Backend/Exercícios de C#/2023-04-12 Exercícios de função/3 - Lista de exercícios 13-04-2023/4 - Maior e menor/Program.cs
@@ -6,6 +6,5 @@ for (var i = 0; i <= 9; i++)
 }
 Array.Sort(numeros);
 Console.WriteLine($"O maior valor informado foi {numeros[9]} e o menos valor foi {numeros[0]}");
-
 // 4 - Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o
 // menor valor lido.
