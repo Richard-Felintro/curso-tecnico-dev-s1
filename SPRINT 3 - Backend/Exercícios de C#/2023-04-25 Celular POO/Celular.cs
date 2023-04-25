8cff84b94bacb30a5397a831eb9aae4e3e1e6f9b
Richard Felintro <richardfelintro06@gmail.com>
2023-04-25 17:25:40 -0300

    Atividade de celular adicionada

---
+++ SPRINT 3 - Backend/Exercícios de C#/2023-04-25 Celular POO/Celular.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Celular_POO
+{
+    public class Celular
+    {
+        public string? modelo;
+        public int tamanho;
+        public string? cor;
+        public bool ligado = false;
+        //* Métodos
+        public void Ligar()
+        {
+            ligado = true;
+        }
+        public bool Desligar(bool ligado)
+        {
+            Console.WriteLine($"Desligando. Pressione ENTER para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+            return false;
+        }
+        public void FazerLigacao()
+        {
+            Console.Clear();
+            Console.WriteLine($"TELEFONE");
+            Console.WriteLine();
+            Console.Write($"Digite o número...");
+            double numeroLigando = int.Parse(Console.ReadLine()!);
+            if (numeroLigando < 100000000 || numeroLigando >= 99999999999)
+            {
+                Console.WriteLine($"Número inválido...");
+            }
+            else
+            {
+                Console.WriteLine($"Ligando...");
+                Console.Write($"Aperte ENTER para continuar...");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"O destinatário achou que era golpe e desligou :(");
+            }
+        }
+        public void EnviarMensagem()
+        {
+            Console.Clear();
+            Console.WriteLine($"Enviando mensagem...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ SPRINT 3 - Backend/Exercícios de C#/2023-04-25 Celular POO/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Celular_POO;
+Celular cell = new Celular();
+
+//*Tela de design do celular
+Console.Clear();
+Console.WriteLine($"Bem-vindo ao Mercado Li-[Nome redactado de acordo com a Lei 9610, artigo 5, inciso I do código penal brasileiro]");
+Console.WriteLine($"Escolha os parametros do seu novo celular!");
+Console.WriteLine();
+Console.Write($"Escolha um modelo... ");
+cell.modelo = Console.ReadLine();
+Console.WriteLine();
+Console.Write($"Escolha o tamanho (polegadas)... ");
+cell.tamanho = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.Write("Escolha a cor do celular...");
+cell.cor = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"Celular criado com sucesso!");
+Console.Write($"Aperte ENTER para continuar...");
+Console.ReadLine();
+Console.Clear();
+cell.ligado = false;
+
+loopInfinito:
+//*Tela de uso do celular
+while (cell.ligado == false)
+{
+    Console.WriteLine($"[1] Ligar celular");
+    Console.WriteLine($"[0] Guardar celular");
+    string desligadoInput = Console.ReadLine();
+    switch (desligadoInput)
+    {
+        case "1":
+            cell.Ligar();
+            break;
+        case "0":
+            Console.Clear();
+            Environment.Exit(0);
+            break;
+        default:
+            Console.Clear();
+            break;
+    }
+}
+
+//* Tela de uso do celular ligado
+while (cell.ligado == true)
+{
+    Console.Clear();
+    Console.WriteLine($"{(cell.modelo!.ToUpper())}");
+    Console.WriteLine();
+    Console.WriteLine("[1] Enviar mensagem");
+    Console.WriteLine("[2] Fazer ligação");
+    Console.WriteLine();
+    Console.WriteLine("[0] Desligar");
+string ligadoInput = Console.ReadLine();
+switch (ligadoInput)
+{
+    case "1":
+        cell.EnviarMensagem();
+        break;
+    case "2":
+        cell.FazerLigacao();
+        break;
+    case "0":
+        cell.ligado = false;
+        break;
+}
+}
+goto loopInfinito;
