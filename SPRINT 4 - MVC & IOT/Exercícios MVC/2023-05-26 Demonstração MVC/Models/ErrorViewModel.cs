2c5dfb88a1d49194f5a15da456ad3740337f65e3
Richard Felintro <richardfelintro06@gmail.com>
2023-05-26 16:16:08 -0300

    Observações e correções MVC.

---
+++ SPRINT 4 - MVC & IOT/Exercícios MVC/2023-05-26 Demonstração MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2023_05_26_Demonstração_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
