58aa08bb7a282e90d268de24f28705674f23b397
Richard Felintro <richardfelintro06@gmail.com>
2023-06-01 17:17:02 -0300

    Funções de manipular equipes adicionadas.

--- SPRINT 4 - MVC & IOT/Exercícios MVC/2023-05-29 Projeto Gamer Fullstack/Controllers/EquipeController.cs
+++ SPRINT 4 - MVC & IOT/Exercícios MVC/2023-05-29 Projeto Gamer Fullstack/Controllers/EquipeController.cs
@@ -33,10 +33,72 @@ namespace projeto_gamer_fullstack.Controllers
         public IActionResult Cadastrar(IFormCollection form)
         {
             Equipe novaEquipe = new Equipe();
-            novaEquipe.Nome = form["Nome"].ToString();
-            //* novaEquipe.Imagem = form["Imagem"].ToString(); Hora de transformar em imagem kkkkk
+            if (form["Nome"].ToString != null)
+            {
+                novaEquipe.Nome = form["Nome"].ToString();
+                //* novaEquipe.Imagem = form["Imagem"].ToString(); Hora de transformar em imagem kkkkk
 
-            //* Upload de imagem:
+                //* Upload de imagem:
+                if (form.Files.Count > 0)
+                {
+                    var file = form.Files[0];
+                    var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+                    if (!Directory.Exists(folder))
+                    {
+                        Directory.CreateDirectory(folder);
+                    }
+
+                    var path = Path.Combine(folder, file.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    novaEquipe.Imagem = file.FileName;
+
+
+                    c.Equipe.Add(novaEquipe);
+                    c.SaveChanges();
+                }
+            }
+            else
+            {
+                novaEquipe.Imagem = "~/wwwroot/img/Equipes/default.png";
+            }
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            Equipe e = c.Equipe.First(e => e.IdEquipe == id);
+
+            c.Equipe.Remove(e);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+
+        [Route("Editar/Id")]
+        public IActionResult Editar(int id)
+        {
+            Equipe e = c.Equipe.First(e => e.IdEquipe == id);
+
+            ViewBag.Equipe = e;
+
+            return View("Edit");
+        }
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection form, Equipe e)
+        {
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.Nome = e.Nome;
+
             if (form.Files.Count > 0)
             {
                 var file = form.Files[0];
@@ -52,9 +114,15 @@ namespace projeto_gamer_fullstack.Controllers
                 {
                     file.CopyTo(stream);
                 }
+                novaEquipe.Imagem = file.FileName;
             }
 
-            c.Equipe.Add(novaEquipe);
+            Equipe equipe = c.Equipe.First(x => x.IdEquipe == e.IdEquipe);
+
+            equipe.Nome = novaEquipe.Nome;
+            equipe.Imagem = novaEquipe.Imagem;
+
+            c.Equipe.Update(equipe);
             c.SaveChanges();
 
             return LocalRedirect("~/Equipe/Listar");
